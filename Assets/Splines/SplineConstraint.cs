320e6e707327929e7c6d715e6c012d3301130e05
Stett <stettjawa@gmail.com>
2015-09-01 01:38:20 -0700

    make a better way of traversing spline segments
    This one still effs up on acute angles

--- Assets/Splines/SplineConstraint.cs
+++ Assets/Splines/SplineConstraint.cs
@@ -3,108 +3,122 @@ using System.Collections;
 
 public class SplineConstraint : MonoBehaviour {
 
-	public SplineData spline;
-	Rigidbody body;
-	ConfigurableJoint joint;
-	int   	vertex_id = 0;
-	float 	vertex_position;
+    public SplineData spline = null;
+    public int   vertex_id = 0;
+    float vertex_position = 0.0f;
+    float vertex_distance = 0.0f;
+    ConfigurableJoint joint;
 
-	// Initialize
-	void Start() {
-		body = GetComponent<Rigidbody> ();
-		joint = gameObject.AddComponent<ConfigurableJoint> ();
-		joint.anchor = new Vector3 (0, 0, 0);
-		joint.autoConfigureConnectedAnchor = false;
-		joint.xMotion = ConfigurableJointMotion.Free;
-		joint.yMotion = ConfigurableJointMotion.Locked;
-		joint.zMotion = ConfigurableJointMotion.Locked;
-		joint.angularXMotion = ConfigurableJointMotion.Locked;
-		joint.angularYMotion = ConfigurableJointMotion.Locked;
-		joint.angularZMotion = ConfigurableJointMotion.Locked;
+    // Initialize
+    void Start() {
+        joint = gameObject.AddComponent<ConfigurableJoint> ();
+        joint.anchor = new Vector3 (0, 0, 0);
+        joint.autoConfigureConnectedAnchor = false;
+        joint.xMotion = ConfigurableJointMotion.Free;
+        joint.yMotion = ConfigurableJointMotion.Locked;
+        joint.zMotion = ConfigurableJointMotion.Locked;
+        joint.angularXMotion = ConfigurableJointMotion.Locked;
+        joint.angularYMotion = ConfigurableJointMotion.Locked;
+        joint.angularZMotion = ConfigurableJointMotion.Locked;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+        ApplyConstraint ();
+    }
 
-	}
+    // Find the closest spline vertex id and position
+    void FindNearestVertex(SplineData spline, int vertex_id, out float vertex_position, out float vertex_distance) {
 
-	// Update is called once per frame
-	void FixedUpdate () {
-		ApplyConstraint ();
-	}
+        // If we don't have a spline, skip this
+        vertex_position = 0.0f;
+        vertex_distance = 0.0f;
+        if (spline == null) return;
 
-	// Find the closest spline vertex id and position
-	void FindNearestVertex() {
-		float min_dist = Mathf.Infinity;
-		for (int i = 1; i < spline.vertexes.Count; i ++) {
-			float dist = min_dist;
-			Vector3 v0 = spline.transform.TransformPoint (spline.vertexes[i-1]);
-			Vector3 v1 = spline.transform.TransformPoint (spline.vertexes[i]);
-			float seg_len_sq = (v1 - v0).sqrMagnitude;
-			
-			// If the segment has no length, return the position and distance
-			// to the first vertex.
-			if (seg_len_sq == 0.0f) {
-				dist = Vector3.Distance (transform.position, v0);
-				if (dist < min_dist) {
-					min_dist = dist;
-					vertex_id = i-1;
-					vertex_position = 0.0f;
-				}
-			} else {
-				float t = Vector3.Dot (transform.position - v0, v1 - v0) / seg_len_sq;
-				
-				// If we are past the first vertex, return the position and
-				// distance to the first vertex.
-				if (t < 0.0f) {
-					dist = Vector3.Distance(transform.position, v0);
-					if (dist < min_dist) {
-						min_dist = dist;
-						vertex_id = i-1;
-						vertex_position = 0.0f;
-					}
-					
-				// If we are past the last vertex, same deal.
-				} else if (t > 1.0f) {
-					dist = Vector3.Distance (transform.position, v1);
-					if (dist < min_dist) {
-						min_dist = dist;
-						vertex_id = i-1;
-						vertex_position = 1.0f;
-					}
-					
-				// We're right in the sweet spot.
-				// Project the position onto the segment.
-				} else {
-					Vector3 projection = v0 + t * (v1 - v0);
-					dist = Vector3.Distance(transform.position, projection);
-					if (dist < min_dist) {
-						min_dist = dist;
-						vertex_id = i-1;
-						vertex_position = t;
-					}
-				}
-			}
-		}
+        // Get the vertex end-points
+        Vector3 v0 = spline.transform.TransformPoint (spline.vertexes[vertex_id]);
+        Vector3 v1 = spline.transform.TransformPoint (spline.vertexes[vertex_id+1]);
 
-	}
+        // Find the closest point to the vertex segement
+        //
+        // TODO: Make this actually calculate closest point on the spline!
+        //       Is it possible?
+        float seg_len_sq = (v1 - v0).sqrMagnitude;
+        vertex_position = Vector3.Dot (transform.position - v0, v1 - v0) / seg_len_sq;
 
-	// Constrain the position to the vertex, and limit the velocity to the direction of the path
-	void ApplyConstraint() {
+        // Get the distance to the vertex
+        if (vertex_position < 0.0f) {
+            vertex_distance = Vector3.Distance(transform.position, v0);
+        } else if (vertex_position > 1.0f) {
+            vertex_distance = Vector3.Distance(transform.position, v1);
+        } else {
+            vertex_distance = Vector3.Distance(transform.position, v0 + (v1 - v0) * vertex_position);
+        }
+    }
 
-		// Update the vertex we should be on
-		FindNearestVertex ();
+    // Constrain the position to the vertex, and limit the velocity to the direction of the path
+    void ApplyConstraint() {
 
-		// Update the joint axis
-		joint.connectedAnchor = spline.transform.TransformPoint (spline.Interpolate (vertex_id, vertex_position));
-		joint.axis = spline.transform.TransformPoint (spline.InterpolateNormal (vertex_id, vertex_position));
-	}
+        // Make sure we have a spline
+        if (spline == null) return;
 
-	// Draw the segment from here to the path we're going to jump to
-	void OnDrawGizmos() {
-		FindNearestVertex ();
-		Gizmos.color = new Color (1.0f, 1.0f, 1.0f, 0.5f);
+        // Update the vertex we should be on
+        FindNearestVertex(spline, vertex_id, out vertex_position, out vertex_distance);
 
-		Vector3 pos = spline.transform.TransformPoint (spline.Interpolate (vertex_id, vertex_position));
-		Vector3 norm = spline.transform.TransformPoint (spline.InterpolateNormal (vertex_id, vertex_position));
+        // If we've gone off one of the ends, try to jump to the next vertex
+        SplineData next_spline = spline;
+        int next_vertex_id = vertex_id;
+        if (vertex_position < 0.0f) {
+            if (vertex_id > 0) {
+                next_vertex_id = vertex_id - 1;
+            } else if (spline.in_connections.Count > 0) {
+                next_spline = spline.in_connections[0];
+                next_vertex_id = spline.in_connections[0].vertexes.Count - 2;
+            } else {
+                spline = null;
+                return;
+            }
+        } else if (vertex_position > 1.0f) {
+            if (vertex_id < spline.vertexes.Count - 2) {
+                next_vertex_id = vertex_id + 1;
+            } else if (spline.out_connections.Count > 0) {
+                next_spline = spline.out_connections[0];
+                next_vertex_id = 0;
+            } else {
+                spline = null;
+                return;
+            }
+        }
 
-		Gizmos.DrawLine (transform.position, pos);
-		Gizmos.DrawLine (pos, pos + norm);
-	}
+        // If we're supposed to try jumping, then fuckin' do it
+        if (next_vertex_id != vertex_id || next_spline != spline) {
+            float next_vertex_position;
+            float next_vertex_distance;
+            FindNearestVertex(next_spline, next_vertex_id, out next_vertex_position, out next_vertex_distance);
+            if (next_vertex_distance < vertex_distance) {
+                spline = next_spline;
+                vertex_id = next_vertex_id;
+                vertex_position = next_vertex_position;
+                vertex_distance = next_vertex_distance;
+            }
+        }
+
+        // Update the joint axis
+        joint.connectedAnchor = spline.transform.TransformPoint(spline.Interpolate(vertex_id, vertex_position));
+        joint.axis = spline.transform.TransformDirection(spline.InterpolateNormal(vertex_id, vertex_position));
+    }
+
+    // Draw the segment from here to the path we're going to jump to
+    void OnDrawGizmos() {
+        if (spline == null) return;
+
+        FindNearestVertex(spline, vertex_id, out vertex_position, out vertex_distance);
+        Gizmos.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        
+        Vector3 pos = spline.transform.TransformPoint(spline.Interpolate(vertex_id, vertex_position));
+        Vector3 norm = spline.transform.TransformDirection(spline.InterpolateNormal(vertex_id, vertex_position));
+
+        Gizmos.DrawLine(transform.position, pos);
+        Gizmos.DrawLine(pos, pos + norm);
+    }
 }
--- Assets/Splines/SplineData.cs
+++ Assets/Splines/SplineData.cs
@@ -4,100 +4,119 @@ using System.Collections.Generic;
 
 
 public class SplineData : MonoBehaviour {
-	
-	public List<Vector3> vertexes;
-	public List<Vector3> tangents;
+    
+    public List<Vector3> vertexes;
+    public List<Vector3> tangents;
+    public List<SplineData> in_connections;
+    public List<SplineData> out_connections;
 
-	// Use this for initialization
-	void Start () {
-		GenerateTangents ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        GenerateTangents ();
 
-	// Generate tangents if needed
-	void GenerateTangents() {
+        // Hook up inputs/outputs
+        foreach (SplineData connection in in_connections)
+            if (!connection.out_connections.Contains(this))
+                connection.out_connections.Add(this);
+        foreach (SplineData connection in out_connections)
+            if (!connection.in_connections.Contains(this))
+                connection.in_connections.Add(this);
+    }
+    
+    // Update is called once per frame
+    void Update () {}
 
-		// If there aren't enough defined tangents, fill them in
-		if (tangents.Count < vertexes.Count && vertexes.Count > 0) {
-			for (int i = tangents.Count; i < vertexes.Count; i ++) {
+    // Generate tangents if needed
+    void GenerateTangents() {
 
-				if (i == 0) {
-					tangents.Add ((vertexes[i+1] - vertexes[i]) * .5f);
-				} else if (i > 0 && i < vertexes.Count - 1) {
-					tangents.Add ((vertexes[i+1] - vertexes[i-1]) * .5f);
-				} else {
-					tangents.Add ((vertexes[i] - vertexes[i-1]) * .5f);
-				}
-			}
-		}
+        // If there aren't enough defined tangents, fill them in
+        if (tangents.Count < vertexes.Count && vertexes.Count > 0) {
+            for (int i = tangents.Count; i < vertexes.Count; i ++) {
 
-		// If there are too many tangents, delete them
-		if (tangents.Count > vertexes.Count) {
-			for (int i = vertexes.Count; i < tangents.Count; i ++) {
-				tangents.RemoveAt(i);
-			}
-		}
-	}
-	
-	// Draw the spline path as a gizmo
-	void OnDrawGizmos() {
+                if (i == 0) {
+                    tangents.Add ((vertexes[i+1] - vertexes[i]) * .5f);
+                } else if (i > 0 && i < vertexes.Count - 1) {
+                    tangents.Add ((vertexes[i+1] - vertexes[i-1]) * .5f);
+                } else {
+                    tangents.Add ((vertexes[i] - vertexes[i-1]) * .5f);
+                }
+            }
+        }
 
-		GenerateTangents ();
+        // If there are too many tangents, delete them
+        if (tangents.Count > vertexes.Count) {
+            for (int i = vertexes.Count; i < tangents.Count; i ++) {
+                tangents.RemoveAt(i);
+            }
+        }
+    }
+    
+    // Draw the spline path as a gizmo
+    void OnDrawGizmos() {
 
-		for (int i = 1; i < vertexes.Count; i ++) {
+        GenerateTangents ();
 
-			// Draw the raw vertex/segment
-			Gizmos.color = new Color (.8f, .8f, .8f, .2f);
-			Vector3 v0 = transform.TransformPoint(vertexes[i-1]);
-			Vector3 v1 = transform.TransformPoint(vertexes[i]);
-			Gizmos.DrawLine (v0, v1);
-			Gizmos.color = new Color (0.5f, 1.0f, 0.7f);
-			if (i == 1)
-				Gizmos.DrawWireSphere(v0, 0.01f);
-			Gizmos.DrawWireSphere(v1, 0.01f);
+        for (int i = 1; i < vertexes.Count; i ++) {
 
-			// Draw the tangent
-			Gizmos.color = new Color (1.0f, 0.5f, 0.2f);
-			if (i == 1)
-				Gizmos.DrawLine (v0, v0 + transform.TransformPoint (tangents[i-1]));
-			Gizmos.DrawLine (v1, v1 + transform.TransformPoint (tangents[i]));
+            // Draw the raw vertex/segment
+            Gizmos.color = new Color (.8f, .8f, .8f, .2f);
+            Vector3 v0 = transform.TransformPoint(vertexes[i-1]);
+            Vector3 v1 = transform.TransformPoint(vertexes[i]);
+            Gizmos.DrawLine (v0, v1);
+            Gizmos.color = new Color (0.5f, 1.0f, 0.7f);
+            if (i == 1)
+                Gizmos.DrawWireSphere(v0, 0.01f);
+            Gizmos.DrawWireSphere(v1, 0.01f);
 
-			// Draw the interpolated segment
-			Vector3 p0 = v0;
-			Vector3 p1;
-			int segments = 10;
-			Gizmos.color = new Color (0.5f, 1.0f, 0.7f);
-			for (int ii = 1; ii <= segments; ii ++) {
-				float t = (float)ii / (float)segments;
-				float t2 = t*t;
-				float t3 = t*t2;
-				p1 = transform.TransformPoint (Interpolate (i-1, t));
-				Gizmos.DrawLine (p0, p1);
-				p0 = p1;
-			}
-		}
-	}
-	
-	// Get an interpolated point
-	public Vector3 Interpolate(int i, float t) {
-		float t2 = t*t;
-		float t3 = t*t2;
-		return (2*t3 - 3*t2 + 1) * vertexes[i]
-			 + (t3 - 2*t2 + t) * tangents[i]
-			 + (-2*t3 + 3*t2) * vertexes[i+1]
-		     + (t3 - t2) * tangents[i+1];
-	}
+            // Draw the tangent
+            Gizmos.color = new Color (1.0f, 0.5f, 0.2f);
+            if (i == 1)
+                Gizmos.DrawLine (v0, v0 + transform.TransformDirection(tangents[i-1]));
+            Gizmos.DrawLine (v1, v1 + transform.TransformDirection(tangents[i]));
 
-	// Get the tangent normal vector at a point on the spline
-	public Vector3 InterpolateNormal(int i, float t) {
-		float t2 = t*t;
-		return ((6*t2 - 6*t) * vertexes[i]
-		      + (3*t2 - 4*t + 1) * tangents[i]
-		      + (-6*t2 + 6*t) * vertexes[i+1]
-		      + (3*t2 - 2*t) * tangents[i+1]).normalized;
-	}
+            // Draw the interpolated segment
+            Vector3 p0 = v0;
+            Vector3 p1;
+            int segments = 10;
+            
+            for (int ii = 1-5; ii <= segments + 5; ii ++) {
+                float t = (float)ii / (float)segments;
+                Gizmos.color = new Color (0.5f, 1.0f, 0.7f);
+                if (t < 0.0f || t > 1.0f)
+                    Gizmos.color = new Color (0.5f, 1.0f, 0.7f, 0.3f);
+                p1 = transform.TransformPoint (Interpolate (i-1, t));
+                Gizmos.DrawLine (p0, p1);
+                p0 = p1;
+            }
+        }
+
+        // Draw end connections
+        Gizmos.color = new Color (1.0f, 0.3f, 0.1f);
+        foreach (SplineData connection in out_connections) {
+            Vector3 v0 = transform.TransformPoint(vertexes[vertexes.Count-1]);
+            Vector3 v1 = connection.transform.TransformPoint(connection.vertexes[0]);
+            Gizmos.DrawLine(v0, v1);
+        }
+    }
+    
+    // Get an interpolated point
+    public Vector3 Interpolate(int i, float t) {
+        if (t < 0.0f) t = 0.0f; else if (t > 1.0f) t = 1.0f;
+        float t2 = t*t;
+        float t3 = t*t2;
+        return (2*t3 - 3*t2 + 1) * vertexes[i]
+             + (t3 - 2*t2 + t) * tangents[i]
+             + (-2*t3 + 3*t2) * vertexes[i+1]
+             + (t3 - t2) * tangents[i+1];
+    }
+
+    // Get the tangent normal vector at a point on the spline
+    public Vector3 InterpolateNormal(int i, float t) {
+        if (t < 0.0f) t = 0.0f; else if (t > 1.0f) t = 1.0f;
+        float t2 = t*t;
+        return ((6*t2 - 6*t) * vertexes[i]
+              + (3*t2 - 4*t + 1) * tangents[i]
+              + (-6*t2 + 6*t) * vertexes[i+1]
+              + (3*t2 - 2*t) * tangents[i+1]).normalized;
+    }
 }
